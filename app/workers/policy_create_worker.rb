9983c084371bce8df20482ff96ea2e3fe82d8249
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-03-15 15:51:03 -0300

    adiciona sneaker service para rodar workers

--- app/workers/policy_create_worker.rb
+++ app/workers/policy_create_worker.rb
@@ -1,14 +1,11 @@
-module Workers
-  class PolicyCreateWorker
-    include Sneakers::Worker
-    from_queue 'policy-created'
-
-    def work(payload)
-      logger.info "### READING PAYLOAD ###"
-      payload = JSON.parse(payload)
-      logger.info "### CREATING POLICY ###"
-      logger.info payload
-      ack!
-    end
-  end
+class PolicyCreateWorker
+   include Sneakers::Worker
+   from_queue 'policy-create'
+   def work(payload)
+     logger.info "### READING PAYLOAD ###"
+     payload = JSON.parse(payload)
+     logger.info "### CREATING POLICY ###"
+     logger.info payload
+     ack!
+   end
 end
