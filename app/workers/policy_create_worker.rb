39b493966195d3a174d57f8991ef87001791496c
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-03-19 10:46:00 -0300

    fix: metodo create

--- app/workers/policy_create_worker.rb
+++ app/workers/policy_create_worker.rb
@@ -6,17 +6,17 @@ class PolicyCreateWorker
     payload = JSON.parse(payload)
     logger.info "### CREATING POLICY ###"
     ActiveRecord::Base.transaction do
-      insured_person = InsuredPerson.create(name: payload["insured_person"]["name"],
+      insured_person = InsuredPerson.create!(name: payload["insured_person"]["name"],
                                             cpf: payload["insured_person"]["cpf"]
       )
 
-      vehicle = Vehicle.create(brand: payload["vehicle"]["brand"],
+      vehicle = Vehicle.create!(brand: payload["vehicle"]["brand"],
                                model: payload["vehicle"]["model"],
                                year: payload["vehicle"]["year"],
                                license_plate: payload["vehicle"]["license_plate"]
       )
 
-      Policy.create(effective_date: payload["effective_date"],
+      Policy.create!(effective_date: payload["effective_date"],
                     expiration_date: payload["expiration_date"],
                     vehicle_id: vehicle.id,
                     insured_person_id: insured_person.id
