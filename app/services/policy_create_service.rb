29e5dd7d607fd435389fa0d594095b634151e9c4
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-04-26 10:36:49 -0300

    muda porta docker

--- app/services/policy_create_service.rb
+++ app/services/policy_create_service.rb
@@ -1,24 +1,24 @@
 class PolicyCreateService
   def initialize(payload)
-  @payload = payload.symbolize_keys
+    @payload = payload.symbolize_keys
   end
 
   def execute!
     ActiveRecord::Base.transaction do
-    Policy.create!(
-        effective_date: @payload[:effective_date],
-        expiration_date: @payload[:expiration_date],
-        insured_person: {
-          name: @payload.dig(:insured_person, "name"),
-          cpf: @payload.dig(:insured_person, "cpf")
-        },
-        vehicle: {
-          brand: @payload.dig(:vehicle, "brand"),
-          model: @payload.dig(:vehicle, "model"),
-          year: @payload.dig(:vehicle, "year"),
-          license_plate: @payload.dig(:vehicle, "license_plate")
-        }
-      )
+      Policy.create!(
+          effective_date: @payload[:effective_date],
+          expiration_date: @payload[:expiration_date],
+          insured_person: {
+            name: @payload.dig(:insured_person, "name"),
+            cpf: @payload.dig(:insured_person, "cpf")
+          },
+          vehicle: {
+            brand: @payload.dig(:vehicle, "brand"),
+            model: @payload.dig(:vehicle, "model"),
+            year: @payload.dig(:vehicle, "year"),
+            license_plate: @payload.dig(:vehicle, "license_plate")
+          }
+        )
     end
   rescue ActiveRecord::RecordInvalid => e
     Rails.logger.info e.message
